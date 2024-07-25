c0139cfdd65d2cc229daf252b981b8aa27ba4627
srvenient <srvenient@gmail.com>
2024-07-25 00:12:47 -0500

    feat: Initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+import re
+
+import setuptools
+
+# for simplicity, we actually store the version in the __version__ attribute in the source
+here = os.path.realpath(os.path.dirname(__file__))
+with open(os.path.join(here, 'app', '__init__.py')) as f:
+    meta_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M)
+    if meta_match:
+        version = meta_match.group(1)
+    else:
+        raise RuntimeError("Unable to find __version__ string.")
+
+with open(os.path.join(here, 'README.md')) as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='Diff Solve',
+    version=version,
+    author='Nelson Rodriguez Roa',
+    author_email="nrodriguezr2006@gmail.com",
+    description='Differential equations solver',
+    long_description=readme,
+    url='https://github.com/srvenient/differential-equations',
+    packages=setuptools.find_packages('app'),
+    install_requires=[
+        'sympy >= 1.13.0',
+        'flask >= 2.0.1'
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
