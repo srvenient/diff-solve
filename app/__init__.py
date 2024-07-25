06618abe550951caa509cde90ff29f3f55992d89
srvenient <srvenient@gmail.com>
2024-07-25 01:01:31 -0500

    fix: an error caused by imports was fixed

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,1 @@
 __version__ = '0.1.0'
-
-
-def create_app():
-    from flask import Flask
-
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
-
-    from . import routes
-    app.register_blueprint(routes.bp)
-
-    return app
